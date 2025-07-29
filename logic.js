7c7e6c851ef21e3ac4aa35a4e167934fc7d9efcc
Herb Segismar <herbert.segismar@yahoo.com>
2025-07-29 22:39:51 +1200

    replace queen svg

--- logic.js
+++ logic.js
@@ -1,16 +1,44 @@
-const gameboard = document.getElementById('gameboard');
+const gameboard = document.getElementById("gameboard");
 const width = 8;
 
 const startPieces = [
-    Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook,
-    Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
-    ...Array(width).fill(null),
-    ...Array(width).fill(null),
-    ...Array(width).fill(null),
-    ...Array(width).fill(null),
-    Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn, Pawn,
-    Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook
-]
+  Rook,
+  Knight,
+  Bishop,
+  Queen,
+  King,
+  Bishop,
+  Knight,
+  Rook,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  ...Array(width).fill(null),
+  ...Array(width).fill(null),
+  ...Array(width).fill(null),
+  ...Array(width).fill(null),
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Pawn,
+  Rook,
+  Knight,
+  Bishop,
+  Queen,
+  King,
+  Bishop,
+  Knight,
+  Rook,
+];
 
 function createBoard() {
   startPieces.forEach((startPiece, index) => {
@@ -24,12 +52,28 @@ function createBoard() {
 
     square.classList.add(isDark ? "dark" : "light");
     square.innerHTML = startPiece;
-    square.setAttribute('square-id', index);
+    square.setAttribute("square-id", index);
+    square.firstChild?.setAttribute("draggable", true);
 
-    {index >= 48 ? square.classList.add('white-piece') : square.classList.add('black-piece')}
-
-    gameboard.appendChild(square);
+    {
+      index >= 48
+        ? square.firstChild.firstChild.classList.add("white")
+        : index <= 15
+        ? square.firstChild.firstChild.classList.add("black")
+        : null;
+    }
+    gameboard.append(square);
   });
 }
 
 createBoard();
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+allSquares.forEach((square) => {
+  square.addEventListener("dragstart", dragStart);
+});
+
+function dragStart(e) {
+  console.log(e.target.parentNode.getAttribute("square-id"));
+}
